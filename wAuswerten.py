edcc88782224a95d361ad89e382409888846a6bf
ChKendel <79915315+ChKendel@users.noreply.github.com>
2023-02-25 23:00:21 +0100

    Update wAuswerten.py

--- wAuswerten.py
+++ wAuswerten.py
@@ -1,7 +1,7 @@
 from wSVG import *
 
 
-oSVG = wSVG("Accelleration_A320_Edinburg.csv", 0, 3)
+oSVG = wSVG("Acceleration_A320_Edinburg.csv", 0, 3)
 
 oSVG.titleY = """Acceleration (m/s<tspan dy ="-1">2</tspan><tspan dy ="1">)</tspan>"""
 oSVG.title = "Messung"
